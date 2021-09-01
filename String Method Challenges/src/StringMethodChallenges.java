76d0f3ea4d69bdd44f557aab83bcc8176395c6da
andrew.markel <andrew.markel@MU-C1-01.mullen.pvt.k12.co.us>
2021-09-01 12:39:40 -0600

    Pig latin inital commit

--- String Method Challenges/src/StringMethodChallenges.java
+++ String Method Challenges/src/StringMethodChallenges.java
@@ -62,8 +62,21 @@ public class StringMethodChallenges {
 		
 		
 		// Pig Latin
-		System.out.println("Input a string and we will return your string in Pig Latin.");
+		System.out.println("\n\nInput a string and we will return your string in Pig Latin.");
 		userString = userInput.nextLine();
+		
+		String userStringArray[] = userString.split(" ");
+//		for (int i = 0; i < userStringArray.length; i++) {
+//			System.out.println(userStringArray[i]);
+//		}
+		System.out.println(userStringArray.length);
+
+		for (int i = 0; i < userStringArray.length; i++) {
+			switch (userStringArray[i]) {
+			case "a": case "e": case "i": case "o": case "u":
+				System.out.println("");
+			}
+		}
 		
 		
 	}
